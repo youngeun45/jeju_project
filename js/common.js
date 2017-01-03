ee40513f6d145ad30f48c2da57e283bd30c25482
ye <ye@ye.ye>
2017-01-03 17:31:53 +0900

    인덱스 작업중

---
+++ js/common.js
@@ -0,0 +1,10 @@
+$(function(){
+	var mSl = ".main_prdt_Slide";
+	$(mSl).bxSlider({
+        auto:true,//auto:true 기본값은 false : 자동슬라이더 
+        autoControls: false, // 정지재생버튼
+        controls:true,
+        pager:true
+	}); 
+
+})
